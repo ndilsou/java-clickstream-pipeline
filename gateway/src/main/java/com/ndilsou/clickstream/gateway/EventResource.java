d4a6e66d8f6a65686ccae5a913b76d6d0b23dfd1
Ndil-Adjim Sou Ngadoy <ndiladjim.soungadoy@trinitymirror.com>
2020-09-09 22:08:19 +0100

    add event enrichment to the gateway main resource

--- gateway/src/main/java/com/ndilsou/clickstream/gateway/EventResource.java
+++ gateway/src/main/java/com/ndilsou/clickstream/gateway/EventResource.java
@@ -1,15 +1,21 @@
 package com.ndilsou.clickstream.gateway;
 
 import com.ndilsou.clickstream.common.events.Event;
+import com.ndilsou.clickstream.gateway.producers.AppendEventResult;
 import com.ndilsou.clickstream.gateway.producers.EventProducer;
-
+import io.atlassian.fugue.Try;
+import io.vertx.core.http.HttpServerRequest;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.core.Response.Status;
 
 
@@ -20,9 +26,28 @@ public class EventResource {
   @Inject
   EventProducer eventProducer;
 
+  @Context
+  UriInfo info;
+
+  @Context
+  HttpServerRequest request;
+
+  /**
+   * Ingest a new event.
+   *
+   * @param event a piece of information you want recorded.
+   * @return
+   */
   @POST
   public Response add(Event event) {
-    var result = eventProducer.append(event);
+
+    AppendEventResult result =
+        validateEvent(event).map(this::enrichEvent).map(eventProducer::append)
+            .fold(e -> AppendEventResult.rejected(e.getMessage()), res -> res);
+    // var validEvent = validateEvent(event);
+    // var enrichedEvent = enrichEvent(validEvent);
+    // var result = eventProducer.append(enrichedEvent);
+
     Response response;
     if (result.isAccepted()) {
       response = Response.status(Status.ACCEPTED).entity(result).build();
@@ -31,4 +56,18 @@ public class EventResource {
     }
     return response;
   }
+
+  private Event enrichEvent(Event event) {
+    // TODO handle X-Forwarded-For when behind load balancer.
+    event.setIpAddress(request.remoteAddress().toString());
+    event.setUserAgent(request.headers().get("User-Agent"));
+    event.setCreatedAt(OffsetDateTime.now(ZoneOffset.UTC));
+    return event;
+  }
+
+  private Try<Event> validateEvent(Event event) {
+    return Try.successful(event);
+  }
+
+
 }
