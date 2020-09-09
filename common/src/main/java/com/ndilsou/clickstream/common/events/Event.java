2a732ffaa5b2739bf897bf04dfd85ef36249703d
Ndil-Adjim Sou Ngadoy <ndiladjim.soungadoy@trinitymirror.com>
2020-09-09 22:06:46 +0100

    extend the event record with server captured data

--- common/src/main/java/com/ndilsou/clickstream/common/events/Event.java
+++ common/src/main/java/com/ndilsou/clickstream/common/events/Event.java
@@ -1,8 +1,11 @@
 package com.ndilsou.clickstream.common.events;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
-
+import com.fasterxml.jackson.databind.node.JsonNodeCreator;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 
@@ -11,10 +14,26 @@ import lombok.Data;
 @Data
 @JsonInclude(JsonInclude.Include.NON_ABSENT)
 public class Event {
-  public String namespace;
-  public String metric;
-  public OffsetDateTime timestamp = OffsetDateTime.now(ZoneOffset.UTC);
-  public Unit unit;
-  public double value;
-  public JsonNode dimensions;
+
+  private String namespace;
+
+  private String metric;
+
+  private OffsetDateTime timestamp = OffsetDateTime.now(ZoneOffset.UTC);
+
+  private Unit unit = Unit.Count;
+
+  private double value = 1.0;
+
+  private JsonNode dimensions = JsonNodeFactory.instance.objectNode();
+
+  @JsonProperty("ip_address")
+  private String ipAddress;
+
+  @JsonProperty("user_agent")
+  private String userAgent;
+
+  @JsonProperty("created_at")
+  private OffsetDateTime createdAt;
+
 }
